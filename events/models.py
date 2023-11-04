4e552f30dab52cd8e49c6a03cee33a3e8df542db
John Willard <willard.john.gray@gmail.com>
2023-11-04 19:10:28 -0400

    add event and guest models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Guest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.name} <{self.email}>"
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    guests = models.ManyToManyField(Guest)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.date}"

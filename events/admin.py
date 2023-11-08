fb3ab17f624e70ca567973aed9815d36d470090c
John Willard <willard.john.gray@gmail.com>
2023-11-07 22:39:21 -0500

    display invitation unique_id in admin

--- events/admin.py
+++ events/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Event, Invitation
 
+
+class InvitationAdmin(admin.ModelAdmin):
+    readonly_fields = ("unique_id",)
+
+
 admin.site.register(Event)
-admin.site.register(Invitation)
+admin.site.register(Invitation, InvitationAdmin)

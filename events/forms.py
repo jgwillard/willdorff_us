ddbc2c1d19b85b19e0fd66d003652c5fbf532aca
John Willard <willard.john.gray@gmail.com>
2023-11-14 22:16:01 -0500

    improve ux

--- events/forms.py
+++ events/forms.py
@@ -7,3 +7,7 @@ class InvitationForm(ModelForm):
     class Meta:
         model = Invitation
         fields = ["is_confirmed", "num_guests"]
+        labels = {
+            "is_confirmed": "Will you be attending?",
+            "num_guests": "How many people are you bringing with you?",
+        }

8d8438ae8202019eff50f74cceeeae9a47024a37
John Willard <willard.john.gray@gmail.com>
2023-11-16 22:28:44 -0500

    improve admin site

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import Contact
 
+admin.site.site_header = "willdorff.us administration"
+
 admin.site.register(Contact)
--- events/admin.py
+++ events/admin.py
@@ -3,9 +3,18 @@ from django.contrib import admin
 from .models import Event, Invitation
 
 
+class InvitationInline(admin.TabularInline):
+    model = Invitation
+    extra = 1
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [InvitationInline]
+
+
 class InvitationAdmin(admin.ModelAdmin):
     readonly_fields = ("unique_id",)
 
 
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Invitation, InvitationAdmin)

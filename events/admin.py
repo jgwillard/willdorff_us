f60b0e1a2b9c53dc711e3b7fb4686176df00404a
John Willard <willard.john.gray@gmail.com>
2023-11-23 21:20:14 -0500

    add email invitations for event action

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,8 @@
-from django.contrib import admin
+from django.conf import settings
+from django.contrib import admin, messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.urls import reverse
 
 from .models import Event, Invitation
 
@@ -18,8 +22,38 @@ class EventAdmin(admin.ModelAdmin):
         for event in queryset:
             # NOTE see https://stackoverflow.com/questions/2005953/access-fields-in-django-intermediate-model
             for invitation in event.invitation_set.all():
-                # TODO send email
-                print(invitation)
+                try:
+                    subject = f"RSVP to {event.name}"
+                    template_name = "events/emails/rsvp_email.html"
+                    link = settings.HOST + reverse(
+                        "rsvp", kwargs={"unique_id": invitation.unique_id}
+                    )
+                    context = {
+                        "subject": subject,
+                        "event": event,
+                        "invitation": invitation,
+                        "link": link,
+                    }
+
+                    html_message = render_to_string(template_name, context)
+                    recipient_list = [invitation.invitee.email]
+                    send_mail(
+                        subject,
+                        "",
+                        settings.DEFAULT_FROM_EMAIL,
+                        recipient_list,
+                        html_message=html_message,
+                        fail_silently=False,
+                    )
+                except Exception as e:
+                    messages.error(
+                        request,
+                        f"Error occurred while trying to email invitation to {invitation.invitee.email}: {str(e)}",
+                    )
+
+            messages.success(
+                request, f"Successfully emailed invitations for {event}"
+            )
 
 
 @admin.register(Invitation)
--- willdorff_us/settings.py
+++ willdorff_us/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +27,8 @@ SECRET_KEY = (
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+HOST = os.environ.get("HOST", default="http://localhost:8000/")
 
 if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"

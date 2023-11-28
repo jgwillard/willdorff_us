c1d25ddb5920af663a0d71d1146efceea199a67e
John Willard <willard.john.gray@gmail.com>
2023-11-27 23:19:54 -0500

    given name and family name should be optional

--- core/models.py
+++ core/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=200)
-    given_name = models.CharField(max_length=200)
-    family_name = models.CharField(max_length=200)
+    given_name = models.CharField(max_length=200, blank=True)
+    family_name = models.CharField(max_length=200, blank=True)
     email = models.CharField(max_length=200)
 
     @property

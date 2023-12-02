aec5920418e442f1fc9cd1a422ca6922ac633f6c
John Willard <willard.john.gray@gmail.com>
2023-12-01 23:58:47 -0500

    added homepage, updated templates

--- willdorff_us/urls.py
+++ willdorff_us/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("core.urls")),
     path("events/", include("events.urls")),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),

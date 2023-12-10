a40007f745df3e8999a6da875aaf75f82910337c
John Willard <willard.john.gray@gmail.com>
2023-12-10 15:02:26 -0500

    test first production bug fix! thanks scott

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,33 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.utils.timezone import now, timedelta
 
-# Create your tests here.
+from core.models import Contact
+from .models import Event, Invitation
+
+
+class InvitationModelTests(TestCase):
+    def setUp(self):
+        # Create necessary instances for ForeignKey fields
+        self.contact = Contact.objects.create(
+            name="John Doe", email="john@example.com"
+        )
+        self.event = Event.objects.create(
+            name="Sample Event",
+            start_time=now(),
+            end_time=now() + timedelta(hours=1),
+        )
+
+    def test_num_guests_cannot_be_negative(self):
+        invitation = Invitation(
+            num_guests=3,
+            invitee=self.contact,
+            event=self.event,
+            is_attending=True,
+        )
+        invitation.full_clean()
+        with self.assertRaisesMessage(
+            ValidationError, "Ensure this value is greater than or equal to 0."
+        ):
+            invitation.num_guests = -1
+            invitation.full_clean()

6e11c3d2ca8235a44fc2f550fa115163c892057a
John Willard <willard.john.gray@gmail.com>
2024-04-03 22:09:46 -0400

    admin action to invite contact list to event

--- events/admin.py
+++ events/admin.py
@@ -2,10 +2,14 @@ from django.conf import settings
 from django.contrib import admin, messages
 from django.core.mail import send_mail
 from django.db import models
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.template.loader import render_to_string
-from django.urls import reverse
+from django.urls import path, reverse
 
 from django_ckeditor_5.widgets import CKEditor5Widget
+
+from core.models import ContactList
 
 from .models import Event, Invitation
 
@@ -21,6 +25,17 @@ class InvitationInline(admin.TabularInline):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [
+            path(
+                "invite-contacts-from-list/",
+                self.invite_contacts_from_list,
+                name="events_event_invite_contacts_from_list",
+            )
+        ]
+        return new_urls + urls
+
     readonly_fields = ("total_expected_guests",)
     formfield_overrides = {
         models.TextField: {
@@ -30,7 +45,7 @@ class EventAdmin(admin.ModelAdmin):
         }
     }
     inlines = [InvitationInline]
-    actions = ["email_invitations"]
+    actions = ["email_invitations", "invite_contacts_from_list"]
 
     @admin.action(description="Email invitations for selected events")
     def email_invitations(self, request, queryset):
@@ -77,6 +92,44 @@ class EventAdmin(admin.ModelAdmin):
             messages.success(
                 request, f"Successfully emailed invitations for {event}"
             )
+
+    @admin.action(
+        description="Invite all contacts on a contact list to selected events"
+    )
+    def invite_contacts_from_list(self, request, queryset=None):
+        if "apply" in request.POST:
+            contact_list_id = int(request.POST.get("contact_list"))
+            contact_list = ContactList.objects.get(pk=contact_list_id)
+            selected_ids = request.POST.getlist("selected_ids")
+            queryset = Event.objects.filter(pk__in=selected_ids)
+            for event in queryset:
+                try:
+                    for contact in contact_list.contacts.all():
+                        Invitation.objects.create(event=event, invitee=contact)
+
+                    messages.success(
+                        request,
+                        f"Successfully created invitations to {event} for {contact_list}",
+                    )
+
+                except Exception as e:
+                    messages.error(
+                        request,
+                        f"Error ocurred when creating invitations to {event} for {contact_list}: {str(e)}",
+                    )
+
+            return HttpResponseRedirect(
+                reverse("admin:events_event_changelist")
+            )
+
+        return render(
+            request,
+            "admin/invite_contacts.html",
+            context={
+                "events": queryset,
+                "contact_lists": ContactList.objects.all(),
+            },
+        )
 
 
 @admin.register(Invitation)
